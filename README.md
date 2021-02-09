d4f6e2eb1873f2a1d3bf0009a0ff98effd41f4cc
vivekmahato <vivekmahato@live.com>
2021-02-09 15:14:45 +0000

    Addition of setup files

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
-# mlots
-PhD Models
+# Machine Learning Over Time-Series (MLOTS)
+
+A toolkit for time-series analysis.
+
+## Placeholder
+
+Work is under progress for the initial public release of the toolkit.
+
+Regards,<br />
+Vivek Mahato.
+
--- setup.py
+++ setup.py
@@ -1,12 +1,29 @@
-from distutils.core import setup
+import os
 
-setup(
+import setuptools
+
+ROOT = os.path.abspath(os.path.dirname(__file__))
+
+with open(os.path.join(ROOT, 'README.md'), encoding="utf-8") as f:
+    long_description = f.read()
+
+setuptools.setup(
     name='mlots',
-    version='0.0.0.0',
-    packages=[''],
-    url='',
-    license='',
-    author='vivekmahato',
-    author_email='vivekmahato@live.com',
-    description='Machine Learning on Time Series'
+    version='0.0.0a10',
+    author="Vivek Mahato",
+    author_email="vivek.mahato@ucdconnect.ie",
+    description="Machine Learning Over Time-Series: A toolkit for time-series analysis",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url='https://github.com/vivekmahato/mlots',
+    packages=setuptools.find_packages(),
+    install_requires=[
+        'tslearn', 'numpy', 'scikit-learn', 'annoy', 'hnswlib', 'sortedcollections',
+        'tqdm'
+    ],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: GNU General Public License v3 or later (GPLv3+)",
+        "Operating System :: OS Independent",
+    ],
 )
