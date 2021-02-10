20b099855072149d01f97b61683d1dda4d368b69
vivekmahato <vivekmahato@live.com>
2021-02-10 23:27:15 +0000

    Restructuring

--- setup.py
+++ setup.py
@@ -9,13 +9,13 @@ with open(os.path.join(ROOT, 'README.md'), encoding="utf-8") as f:
 
 setuptools.setup(
     name='mlots',
-    version='0.0.3',
+    version='0.0.3.1',
     author="Vivek Mahato",
     author_email="vivek.mahato@ucdconnect.ie",
     description="Machine Learning Over Time-Series: A toolkit for time-series analysis",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url='https://github.com/vivekmahato/mlots',
+    url='http://mlots.readthedocs.io/',
     packages=["mlots"],
     install_requires=[
         'tslearn', 'numpy', 'scikit-learn', 'annoy', 'hnswlib', 'sortedcollections',
