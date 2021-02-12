c2f4cbeba83a17b70d82ad6df073bb4de6b572bb
Vivek Mahato <vivek.mahato@ucdconnect.ie>
2021-02-12 21:44:05 +0000

    fixing structure

--- docs/conf.py
+++ docs/conf.py
@@ -22,7 +22,7 @@ copyright = '2021, Vivek Mahato'
 author = 'Vivek Mahato'
 
 # The full version, including alpha/beta/rc tags
-release = '0.0.4'
+release = '0.0.4.1'
 
 # -- General configuration ---------------------------------------------------
 
--- mlots/__init__.py
+++ mlots/__init__.py
@@ -1,2 +1,1 @@
-from .models import *
-from .utilities import *
+__all__ = ["models", "utilities"]
